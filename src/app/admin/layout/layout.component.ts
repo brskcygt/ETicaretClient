f900db51c14601fe569b526c7af895db61e06e74
brskcygt <bariskoc.249@gmail.com>
2023-08-31 12:34:45 +0300

    a service for alertifyjs prepared

--- src/app/admin/layout/layout.component.ts
+++ src/app/admin/layout/layout.component.ts
@@ -1,13 +1,15 @@
-import { Component } from '@angular/core';
-declare var alertify: any;
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
   styleUrls: ['./layout.component.scss']
 })
-export class LayoutComponent {
+export class LayoutComponent implements OnInit {
+  constructor() {}
 
+  ngOnInit(): void {
+  }
 }
 
-alertify.alert('test');
+
--- src/app/services/admin/alertify.service.ts
+++ src/app/services/admin/alertify.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+declare var alertify: any;
+
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +8,41 @@ import { Injectable } from '@angular/core';
 export class AlertifyService {
 
   constructor() { }
+
+  message(message: string, options: Partial<AlertifyOptions>) {
+    alertify.set('notifier','delay', options.delay);
+    alertify.set('notifier','position', options.position);
+    var msg = alertify[options.messageType](message);
+    if(options.dismissOthers){
+      msg.dismissOthers();
+    }
+  }
+
+  dismiss(){
+    alertify.dismissAll();
+  }
+}
+
+export class AlertifyOptions {
+  messageType: MessageType = MessageType.MESSAGE;
+  position: Position = Position.BOTTOM_LEFT;
+  delay: Number = 3;
+  dismissOthers: boolean = false;
+}
+
+export enum MessageType {
+  ERROR = 'error',
+  MESSAGE = 'message',
+  NOTIFY = 'notify',
+  SUCCESS = 'success',
+  WARNING = 'warning'
+}
+
+export enum Position {
+  TOP_LEFT = 'top-left',
+  TOP_RIGHT = 'top-right',
+  TOP_CENTER = 'top-center',
+  BOTTOM_LEFT = 'bottom-left',
+  BOTTOM_RIGHT = 'bottom-right',
+  BOTTOM_CENTER = 'bottom-center'
 }
