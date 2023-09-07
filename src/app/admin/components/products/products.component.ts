bf6ef5a022d07214dd127153fd5570a7cf21a231
brskcygt <bariskoc.249@gmail.com>
2023-09-07 21:50:11 +0300

    .

--- src/app/admin/components/products/products.component.ts
+++ src/app/admin/components/products/products.component.ts
@@ -57,19 +57,6 @@ export class ProductsComponent extends BaseComponent implements OnInit {
       stock: 50,
       price: 2.5
     }).subscribe();
-    this.httpClientService.post({
-      controller:"products"
-    },{
-      name: 'Silgi',
-      stock: 50,
-      price: 2.5
-    }).subscribe();
-    this.httpClientService.post({
-      controller:"products"
-    },{
-      name: 'Silgi',
-      stock: 50,
-      price: 2.5
-    }).subscribe();
+    
   }
 }
